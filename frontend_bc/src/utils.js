7654e3cf06c7c7f98f2c23e6ed8c138c3e90937e
nopeakbar <114820984+nopeakbar@users.noreply.github.com>
2025-05-22 15:53:06 +0700

    Delete frontend_bc directory

--- frontend_bc/src/utils.js
+++
@@ -1,9 +0,0 @@
-// src/utils.js
-import axios from 'axios';
-
-export const BASE_URL = 'https://backend-notes-vey-103949415038.us-central1.run.app';
-
-export const API = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true,
-});
